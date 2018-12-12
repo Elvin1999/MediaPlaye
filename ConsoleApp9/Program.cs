9475b37da9949b817811cf327929e40a80f4c1b2
camalzade_elvin <camalzade_elvin@outlook.com>
2018-12-12 21:11:00 +0400

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp9
+{
+    interface IMedia
+    {
+        string GetDuration();
+        string GetFormat();
+        string GetFile();
+    }
+    class BaseMedia : IMedia
+    {
+        public string GetDuration()
+        {
+            return "";
+        }
+
+        public string GetFile()
+        {
+            return "";
+        }
+
+        public string GetFormat()
+        {
+            return "";
+        }
+    }
+    class Music : BaseMedia
+    {
+        bool IsMono()
+        {
+            return false;
+        }
+        bool IStereo()
+        {
+            return false;
+        }
+    }
+    class Video : Music
+    {
+        void AddFrame(Frame frame)
+        {
+
+        }
+        string GetFrame(int second)
+        {
+            return " ";
+        }
+        int FramesCount()
+        {
+            return 1;
+        }
+    }
+    class Frame
+    {
+        int GetSecond()
+        {
+            return 1;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
