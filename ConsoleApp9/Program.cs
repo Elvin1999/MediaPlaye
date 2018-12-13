ec53701287c2eb7991b8ffb60e2d5fe2184fb709
camalzade_elvin <camalzade_elvin@outlook.com>
2018-12-13 19:05:09 +0400

    Mediaplayer project is ready . . .

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -13,59 +13,119 @@ namespace ConsoleApp9
     }
     class BaseMedia : IMedia
     {
+        protected BaseMedia(string duration, string format, string filePath)
+        {
+            Duration = duration;
+            Format = format;
+            FilePath = filePath;
+        }
+        public string Duration { get; set; }
+        public string Format { get; set; }
+        public string FilePath { get; set; }
         public string GetDuration()
         {
-            return "";
+            return Duration;
         }
 
         public string GetFile()
         {
-            return "";
+            return FilePath;
         }
 
         public string GetFormat()
         {
-            return "";
+            return Format;
         }
     }
     class Music : BaseMedia
     {
-        bool IsMono()
+        public bool Mono { get; set; }
+        public bool Stereo { get; set; }
+        public bool IsMono()
         {
-            return false;
+            return !Stereo;
         }
-        bool IStereo()
+        public bool IStereo()
         {
-            return false;
+            return Stereo;
+        }
+        public Music(string duration, string format, string filePath,bool stereo)
+            : base(duration, format, filePath)
+        {
+            Stereo = stereo;
         }
     }
     class Video : Music
     {
-        void AddFrame(Frame frame)
+        List<Frame> frames = new List<Frame>();
+        public Video(string duration, string format, string filePath,bool stereo) :
+            base(duration, format, filePath,stereo)
         {
-
         }
-        string GetFrame(int second)
+        public void AddFrame(Frame frame)
         {
-            return " ";
+            frames.Add(frame);
         }
-        int FramesCount()
+        public string GetFrame(int second)
         {
-            return 1;
+            return frames[second - 1].MyFrame;
+        }
+        public int FramesCount()
+        {
+            return frames.Count;
+        }
+        public void ShowVideo()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("===================================================================");
+            Console.WriteLine($"Duration {Duration} Format {Format} FilePath {FilePath} Stereo {IStereo()}  Mono {IsMono()}");
+            Console.WriteLine("===================================================================");
+            foreach (var item in frames)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine("=========");
+                Console.WriteLine("=========");
+                Console.WriteLine($"|{item.MyFrame}|");
+                Console.WriteLine("=========");
+                Console.WriteLine("=========");
+            }
         }
     }
     class Frame
     {
-        int GetSecond(int framesecond)
+        public int Second { get; set; }
+        public string MyFrame { get; set; }
+        public Frame(string myFrame,int second)
         {
-            return 1;
+            MyFrame = myFrame;
+        }  
+        int GetSecond()
+        {
+            return Second;
+        }
+    }
+    class Controller
+    {
+        public void Run()
+        {
+            Frame frame1 = new Frame("1.Frame",1);
+            Frame frame2 = new Frame("2.Frame",2);
+            Frame frame3 = new Frame("3.Frame",3);
+            Frame frame4 = new Frame("4.Frame",4);
+            Music music = new Music("2.55", "mp3", @"C:\Users", true);    
+            Video video = new Video("2.55", "mp4", @"C:\Users", true);          
+            video.AddFrame(frame1); video.AddFrame(frame2);
+            video.AddFrame(frame3); video.AddFrame(frame4);
+            //Console.WriteLine(video.GetFrame(3));
+            video.ShowVideo();
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-
+            Controller controller = new Controller();
+            controller.Run();
         }
     }
 }
